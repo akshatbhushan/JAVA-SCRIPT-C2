5868c305a35550ea3b3c482270d3df1fe7db712d
Akshat Bhushan <ak2111bh@gmail.com>
2026-03-11 19:02:57 +0530

    Day 29 C2

---
+++ JS4 (L-29)/script.js
@@ -0,0 +1,33 @@
+// 1
+//  console.log ("Connected")
+
+// 2
+// alert ('Yo')
+
+// 3
+//  var a = "Ben"
+// let b = "Tennyson"
+// const c = "Hero"
+
+// 4
+//  a = "Updated"
+// b = "Updated"
+// // c = "Updated"
+
+// 5
+// console.log ("Ben");
+// console.info ("16");
+// console.warn ("USA");
+// console.error ("USA");
+
+//6
+//  let naam = prompt('Your Name?')
+// alert (`Hello ${naam}`)
+// console.log(typeof naam)
+
+//7
+ let msg = 'I Love Sheryians'
+// msg.replace("Love", "study at")
+console.log(msg.replace("Love", "study at"))
+// msg.split(” “)
+// console.log(msg)
