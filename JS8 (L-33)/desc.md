cd5c797bca9c32e1d5d378b8f77a501362d4f58c
Akshat Bhushan <ak2111bh@gmail.com>
2026-03-21 17:29:26 +0530

    name changed

---
+++ JS8 (L-33)/desc.md
@@ -0,0 +1,1 @@
+Loops and Conditional practice 33 J8
---
+++ JS8 (L-33)/script.js
@@ -0,0 +1,73 @@
+// 👉🏻 1. numbers 1-10
+
+// for ( i=1 ; i >=10 ; i++) {
+//     console.log(i)
+// }
+
+//👉🏻 2 Even 1-20
+
+// for ( i = 1; i<21 ; i++) {
+//     if (i%2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+// for ( i =2 ; i < 21 ; i+=2) {
+//     console.log(i)  Hell Naah 
+// }
+
+// 👉🏻 3 10-1
+
+// for ( i = 10; i>0 ; i--) {
+//     console.log(i)
+// }
+
+// 👉🏻 4 repeat yes 5X
+
+// for (i = 111 ; i <=115 ; i++){
+//     console.log("Yes")
+// }
+
+// // 👉🏻 5. Odd even check 1-10 
+
+// for ( i = 1 ; i < 11 ; i ++ ) {
+//     if (i%2 === 0 ){
+//         console.log ( `${i} is even.`)
+//     } else { 
+//         console.log ( `${i} is odd.`)
+//     }
+// } 
+
+// // OR
+
+// for ( i = 1 ; i < 11 ; i ++ ) {
+//     if (i%2 === 0 ) console.log ( `${i} is even.`)
+//         else console.log ( `${i} is odd.`)
+    
+// } 
+
+// 👉🏻 6. Number +ve or -ve condi
+
+// let x = +prompt ('Enter no.')
+// if ( x > 0 ) console.log( "+ve")
+//     else if (x === 0) console.log ("Zero")
+//     else console.log("-ve")
+
+
+let i = + prompt ('Enter no.')
+if ( i > 0 ) {
+    console.log( "+ve")
+} else if (i === 0) { 
+    console.log("Zero")
+} else {
+        console.log("-ve")
+    }
+
+
+// let i = +prompt ('Enter no.')
+// if ( i > 0 ) {
+//     console.log( "+ve")
+// }
+//     else {
+//         console.log("-ve")
+//     }
